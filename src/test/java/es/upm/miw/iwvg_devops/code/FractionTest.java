cdc45133bf24fc8ae0512e56d2ed1c581d4de09b
MMartinHornillos <38863159+MMartinHornillos@users.noreply.github.com>
2020-09-24 20:29:32 +0200

    added new methods to Fraction.java with their respective tests FractionTest.java #9

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -33,4 +33,49 @@ public class FractionTest {
         assertEquals("Fraction{numerator=3, denominator=4}", objectToString);
     }
 
+    @Test
+    void testIsProper() {
+        Fraction fraction = new Fraction(4, 5);
+        assertTrue(fraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        Fraction fraction = new Fraction(6, 5);
+        assertTrue(fraction.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        Fraction fraction = new Fraction(2, 3);
+        assertTrue(fraction.isEquivalent(new Fraction(4, 6)));
+    }
+
+    @Test
+    void testAdd() {
+        Fraction fraction1 = new Fraction(1, 5);
+        Fraction fraction2 = new Fraction(2, 10);
+        Fraction fraction3 = fraction1.add(fraction2);
+        assertEquals(fraction3.getNumerator(), 20);
+        assertEquals(fraction3.getDenominator(), 50);
+    }
+
+    @Test
+    void testMultiply() {
+        Fraction fraction1 = new Fraction(1, 5);
+        Fraction fraction2 = new Fraction(2, 3);
+        Fraction fraction3 = fraction1.multiply(fraction2);
+        assertEquals(fraction3.getNumerator(), 2);
+        assertEquals(fraction3.getDenominator(), 15);
+    }
+
+    @Test
+    void testDivide() {
+        Fraction fraction1 = new Fraction(1, 5);
+        Fraction fraction2 = new Fraction(2, 3);
+        Fraction fraction3 = fraction1.divide(fraction2);
+        assertEquals(fraction3.getNumerator(), 3);
+        assertEquals(fraction3.getDenominator(), 10);
+    }
+
 }
